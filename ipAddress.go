ad79ded2015f931e7b3a999c51f298514679d0c2
Saurabh Agarwal <saurabhagg301@gmail.com>
2020-04-05 17:29:32 +0530

    Host IP address

---
+++ ipAddress.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	res := resolveHostIp()
+	fmt.Println(res)
+}
+
+func resolveHostIp() string {
+
+	netInterfaceAddresses, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return ""
+	}
+
+	for _, netInterfaceAddress := range netInterfaceAddresses {
+
+		networkIp, ok := netInterfaceAddress.(*net.IPNet)
+
+		if ok && !networkIp.IP.IsLoopback() && networkIp.IP.To4() != nil {
+
+			ip := networkIp.IP.String()
+
+			fmt.Println("Resolved Host IP: " + ip)
+
+			return ip
+		}
+	}
+	return ""
+}
